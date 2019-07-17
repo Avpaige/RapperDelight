4e8337c684a23006fa8f652ce9ec17a2a89a96cd
Ari Virgil-Paige <avpaige@gmail.com>
2019-07-16 22:25:50 -0700

    js and html framework

--- assets/javascript/rap.js
+++ assets/javascript/rap.js
@@ -3,46 +3,60 @@ $(document).ready(function(){
 
 var rappers = ["Drake", "Lauryn Hill", "Jay Z", "Nas", "Lizzo", "Missy Elliot", "Jidenna", "Childish Gambino", "J. Cole", "The Game", "Lupe Fisasco", "The Roots", "Chill Moody",];
 
+
+function call (){
+
 var rapper = $(this).attr("data-name");
 var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=L2oUAp0AqJ5LV9sRw8nN5HGNGRiSJ1SP&tag=" + rapper + "&limit:10";
 
-
-
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function(response) {
-
-//   var results = response.data;
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
   
-//   for ( var i =0; i< results.length; ii++){
+    var results = response.data;
+    var rating = results[i].rating;
+  
+    for ( var i =0; i< results.length; ii++){
+   
+        var p = $("<p>").text("Rating: " + rating);
+        $(".gifs").append(results);
+        $(".gifs").prepend(p);
+        }
+});
 
-//     var rating = results[i].rating;
-//     var p = $("<p>").text("Rating: " + rating);
+    function getButtons() {
 
-//     var still = 
+  $(".buttons").empty();
 
+  for (var i = 0; i < rappers.length; i++) {
 
+    var newButton = $("<button>").addClass("gifButton")
+    // .attr("data-name", rappers[i]).text(rappers[i]);
+    
+    $(".buttons").append(newButton);
 
-
-// }
-  function getButtons() {
-
-    $(".buttons").empty();
-
-    for (var i = 0; i < rappers.length; i++) {
-
-      var newButton = $("<button>").addClass("gifButton")
-      // .attr("data-name", rappers[i]).text(rappers[i]);
-      
-      $(".buttons").append(newButton);
-
-    }
   }
+}
 
-getButtons();
+$("#formBtn").on("click"), function(evt){
+  evt.preventDefault();
+  var added = $("#addRap").val().trim();
+  rappers.push(added);
+  
+  getButtons();
+}
+
+ 
+
 
 }
+
+$(document).on("click", ".gifButton", call);
+getButtons();
+
+
+
 // loop through buttons to create dynamic buttons based on giphs
 //clear div before button is pressed
 //add new buttons based on search bar
