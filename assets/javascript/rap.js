00b9e8e1d24cf48d16e67b5b510cabed8e7fd13d
Ari Virgil-Paige <avpaige@gmail.com>
2019-07-19 21:56:21 -0700

    updates to jquery for rating

--- assets/javascript/rap.js
+++ assets/javascript/rap.js
@@ -1,21 +1,17 @@
 $(document).ready(function(){
     
-
 var rappers = ["Sugar Hill Gang","Drake", "Slick Rick", "Lauryn Hill", "Jay-Z", "Nas", "Lizzo", "Missy Elliot", "Jidenna", "Childish Gambino", "J. Cole", "The Game", "Lupe Fisasco", "The Roots",];
 
-    function getButtons() {
+function getButtons() {
 
   $(".buttons").empty();
 
   for (var i = 0; i < rappers.length; i++) {
-
     var newButton = $("<button>");
-    
         newButton.addClass("gifButn");
         newButton.attr("data-name", rappers[i]);
         newButton.text(rappers[i]);
         $(".buttons").append(newButton);
-
   }
 }
 
@@ -23,26 +19,28 @@ var rappers = ["Sugar Hill Gang","Drake", "Slick Rick", "Lauryn Hill", "Jay-Z", 
 getButtons();
 
   $(this).on("click", function() {
-  var rapper = $(this).attr("data-name");
+  var search = $(this).attr("data-name");
   console.log(this)
-  console.log(rapper)
+  console.log(search)
   
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + rapper + "&api_key=L2oUAp0AqJ5LV9sRw8nN5HGNGRiSJ1SP&limit=10";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=L2oUAp0AqJ5LV9sRw8nN5HGNGRiSJ1SP&limit=10";
+  console.log(queryURL  + " is the url")
 
-   
   $.ajax({
       url: queryURL,
       method: "GET"
   }).then(function(response) {
-      var results = response.date;
-
-      for (var i = 0; i < results.length; i++) {
+      var results = response.data;
+      // var rating = results.rating;
+     for (var i = 0; i < results.length; i++) {
         var rapGifs = $("<img>");
-        rap.Gifs.attr("src", results[i].images.fixed_height.url)
-
+        // rapGifs.attr("Rating", rating);
+        rapGifs.attr("src", results[i].images.fixed_height.url)
+        rapGifs.addClass("ajaxGifs")
         $("#gifDiv").prepend(rapGifs);
         console.log(results)
-        console.log(rapGifs)
+        // console.log(rapGifs)
+        // + "Rating: " + rating
       }
     });     
   });
